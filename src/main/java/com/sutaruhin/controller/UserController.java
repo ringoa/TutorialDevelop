e086c3bce47ceff66d2dcc862d60f9be3df3b137
ringoa <84253928+ringoa@users.noreply.github.com>
2025-09-23 14:44:13 +0100

    入力チェックに関する処理の作成

--- src/main/java/com/sutaruhin/controller/UserController.java
+++ src/main/java/com/sutaruhin/controller/UserController.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,7 +37,11 @@ public class UserController {
 	}
 
 	@PostMapping("/register")
-	public String postRegister(User user) {
+	public String postRegister(@Validated User user, BindingResult res, Model model) {
+		if(res.hasErrors()) {
+			
+			return getRegister(user);
+		}
 		service.saveUser(user);
 
 		return "redirect:/user/list";
--- src/main/java/com/sutaruhin/entity/User.java
+++ src/main/java/com/sutaruhin/entity/User.java
@@ -1,5 +1,7 @@
 package com.sutaruhin.entity;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,6 +10,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 
@@ -28,18 +35,24 @@ public class User {
 
 
     @Column(length = 20, nullable = false)
+    @NotEmpty
+    @Length(max=20)
     private String name;
 
 
     @Column(length = 2)
     @Enumerated(EnumType.STRING)
+    @NotNull
     private Gender gender;
 
-
+    @Min(0)
+    @Max(120)
     private Integer age;
 
 
     @Column(length = 50)
+    @Email
+    @Length(max=50)
     private String email;
 
 }
